8663695bec4efbfb8641c221a16960f46fd6e4bf
fiddle-thomas <code.fiddle@gmail.com>
2024-07-08 23:56:12 +0530

    Update Collatz sequence solution

--- solutions/chapter-03-the-collatz-sequence.py
+++ solutions/chapter-03-the-collatz-sequence.py
@@ -1,1 +1,14 @@
-# 
+def collatz(number):
+    if number % 2 == 0:
+        return number // 2
+    else:
+        return 3 * number + 1
+
+try:
+    num = int(input("Enter number: "))
+except ValueError:
+    print("You must enter an integer.")
+else:
+    while num != 1:
+        num = collatz(num)
+        print(num)
