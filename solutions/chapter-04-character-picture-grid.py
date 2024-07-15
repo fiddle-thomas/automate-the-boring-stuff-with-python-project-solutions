60780a79623e1bedc72e5f7a93463dfc9c323239
fiddle-thomas <code.fiddle@gmail.com>
2024-07-15 12:18:03 +0530

    Update solution for chapter-04-character-picture-grid.py

--- solutions/chapter-04-character-picture-grid.py
+++ solutions/chapter-04-character-picture-grid.py
@@ -1,1 +1,18 @@
-#
+grid = [['.', '.', '.', '.', '.', '.'],
+        ['.', 'O', 'O', '.', '.', '.'],
+        ['O', 'O', 'O', 'O', '.', '.'],
+        ['O', 'O', 'O', 'O', 'O', '.'],
+        ['.', 'O', 'O', 'O', 'O', 'O'],
+        ['O', 'O', 'O', 'O', 'O', '.'],
+        ['O', 'O', 'O', 'O', '.', '.'],
+        ['.', 'O', 'O', '.', '.', '.'],
+        ['.', '.', '.', '.', '.', '.']]
+
+# Iterate over each column
+for y in range(len(grid[0])):
+    # Iterate over each row
+    for x in range(len(grid)):
+        # Print the character at (x, y) without a newline or space
+        print(grid[x][y], end="")
+    # Move to the next line after each column is printed
+    print()
